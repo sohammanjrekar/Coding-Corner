06f87c3668188818988c78b03d3692842a497548
soham shrikant manjrekar <91745711+sohammanjrekar@users.noreply.github.com>
2024-11-26 17:54:48 +0530

    upload

--- C/hello.c
+++ C/hello.c
@@ -1,9 +1,11 @@
-#include<stdio.h>
-int main(){
-    printf("HEllo world! \n"); 
-   
-    int i; // Declared but not initialized
+#include <stdio.h>
+int main()
+{
+    printf("HEllo world! %d \n", printf(" soham "));
+    int i;                         // Declared but not initialized
     printf("Value of i: %d\n", i); // Prints a random (garbage) value
+    int x = 10;
+    typeof(x) y = 20; 
+    printf("x: %d, y: %d\n", x, y);
     return 0;
-
 }
