42f0ddbe81359670c8148242aa37bed0649dac44
soham shrikant manjrekar <91745711+sohammanjrekar@users.noreply.github.com>
2024-12-04 19:49:24 +0530

    uplaod

---
+++ cpp/dataTypes.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int n=12;
+    float x=0.235;
+    char c='l';
+    double d=123.456789;
+    string str="soham";
+    bool b=true;
+    cout<<"variables type :"<<sizeof(int)<<endl;
+    return 0;
+}
---
+++ cpp/hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"hello world!\n";
+    int i;
+    cin>>i;
+    cout<<"you entered: "<<i;
+    int a,b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    cout<<"Sum: "<<a+b<<endl;
+    cout<<"Difference: "<<a-b<<endl;
+    cout<<"Product: "<<a*b<<endl;
+    cout<<"Division: "<<a/b<<endl;
+    cout<<"Modulo: "<<a%b<<endl;
+    return 0; 
+}
